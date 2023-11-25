9db3ad813f77d34fb087896333c98ba61cdd25b8
Shakezidin <sinuzidin@gmail.com>
2023-11-25 22:21:28 +0530

    completed

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ func main() {
 
 	resultChannel := make(chan model.ConvertedMessage)
 
+	go worker(requestChannel, resultChannel)
+
 	r.POST("/submit", func(c *gin.Context) {
 		var req model.Request
 
@@ -33,3 +35,31 @@ func main() {
 	r.Run(":8080")
 }
 
+func worker(requests <-chan model.Request, results chan<- model.ConvertedMessage) {
+	req := <-requests
+	converted := convertRequest(req)
+	results <- converted
+}
+
+func convertRequest(req model.Request) model.ConvertedMessage {
+	return model.ConvertedMessage{
+		Event:           req.Ev,
+		EventType:       req.Et,
+		AppID:           req.Id,
+		UserID:          req.Uid,
+		MessageID:       req.Mid,
+		PageTitle:       req.T,
+		PageURL:         req.P,
+		BrowserLanguage: req.L,
+		ScreenSize:      req.Sc,
+		Attributes: map[string]model.Attribute{
+			"form_varient": {Value: req.Atrv1, Type: req.Atrt1},
+			"ref":          {Value: req.Atrv2, Type: req.Atrt2},
+		},
+		UserTraits: map[string]model.Trait{
+			"name":  {Value: req.Uatrv1, Type: req.Uatrt1},
+			"email": {Value: req.Uatrv2, Type: req.Uatrt2},
+			"age":   {Value: req.Uatrv3, Type: req.Uatrt3},
+		},
+	}
+}
